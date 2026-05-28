15b36d744604ab4401258053c34dd0434ab5ef0d
Rathan Kumar <developer.rathan@gmail.com>
2026-05-28 13:57:27 +0530

    support agent and manager agent functionalities

--- dj_ai_employee_main/urls.py
+++ dj_ai_employee_main/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 
     path('orders/', include('orders.urls')),
+
+    path('support/', include('support.urls')),
 ]
